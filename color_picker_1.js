5886a51ebab7ff5c154aef58212654d5d23f055e
lokeshlokesh2121 <20mh1a0489@acou.edu.in>
2024-09-10 18:52:31 +0530

    first commit

---
+++ color_picker_1.js
@@ -0,0 +1,24 @@
+let color_back_ground_element=document.getElementById("back_ground_element");
+let color_code_elememt=document.getElementById("colorcodeelement");
+
+function button1(){
+    color_back_ground_element.style.backgroundColor="#e0e0e0";
+    color_code_elememt.textContent="#e0e0e0";
+    color_code_elememt.style.color="#e0e0e0";
+
+}
+function button2(){
+    color_back_ground_element.style.backgroundColor="#6fcf97";
+    color_code_elememt.textContent="#6fcf97";
+    color_code_elememt.style.color="#6fcf97";
+}
+function button3(){
+    color_back_ground_element.style.backgroundColor="#56ccf2";
+    color_code_elememt.textContent="#56ccf2";
+    color_code_elememt.style.color="#56ccf2";
+}
+function button4(){
+    color_back_ground_element.style.backgroundColor="#bb6bd9";
+    color_code_elememt.textContent="#bb6bd9";
+    color_code_elememt.style.color="#bb6bd9";
+}
